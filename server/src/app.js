89db6e495e6a1245c6286c58021886f392556b66
suthakaranburaj <suthakaranburaj@gmail.com>
2025-12-31 02:04:06 +0530

    cors

--- server/src/app.js
+++ server/src/app.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(
     cors({
         origin: [
-            "https://aganitha-assignment-ztuw.onrender.com",
+            "https://aganitha-assignment-sigma.vercel.app/",
             "http://localhost:3000"
         ],
         credentials: true
